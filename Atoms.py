011e66ad426cb15cd05a400fae0ec32fb6fb53b9
Tiernan Blain <s2729376@ed.ac.uk>
2026-01-30 16:01:00 +0000

    Decay and print function operating, need to add time step next

--- Atoms.py
+++ Atoms.py
@@ -1,17 +1,31 @@
 import numpy as np
 import random 
 
+
 class Atoms():
     def __init__(self, atoms, lamda):
         self.atoms = atoms
         self.lamda = lamda 
+
+    def __str__(self):
+        n = int(np.sqrt(len(self.atoms)))
+        grid = self.atoms.reshape(n, n)
+        output = ""
+        for row in grid:
+            for point in row:
+                output += f"{point} "
+            output += "\n"
+        
+        return output
 
     
 
     def decay(self):
         """simulates decay of the atoms"""
         count = 0 
-        while count < len(self.atoms)/2: #loop until count is half of the total
+
+
+        while count < len(self.atoms)//2: #loop until count is half of the total
             for i in range(len(self.atoms)): #loop for all atoms 
                 if self.atoms[i] != 0: #if atom hasnt already decayed
                     if random.random() < self.lamda: #decide if it should decay
@@ -22,17 +36,18 @@ class Atoms():
                 else:
                     pass
 
-        return self.atoms
-         
+       
+def main():
+    lamda = 0.2
+    array = Atoms(np.ones(25, dtype=int),  lamda)
+    array.decay()
+    print(array)
+
 
 
 if __name__ == "__main__":
+    main()
     
-    array = Atoms(np.ones((5,5), dtype=int))
-    lamda = 0.2
 
-    a1 = array.decay(lamda)
-
-    print(a1)
 
 
