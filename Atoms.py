6d206bd0e731122a94f20704934d26afdb4d7841
Tiernan Blain <s2729376@ed.ac.uk>
2026-02-01 21:06:13 +0000

    timestep added to calculate simulated halflife

--- Atoms.py
+++ Atoms.py
@@ -3,19 +3,23 @@ import random
 
 
 class Atoms():
-    def __init__(self, atoms, lamda):
+    def __init__(self, atoms, lamda, step):
+        "initialise the array, lamda, and the time step"
         self.atoms = atoms
         self.lamda = lamda 
+        self.step = step
 
     def __str__(self):
-        n = int(np.sqrt(len(self.atoms)))
-        grid = self.atoms.reshape(n, n)
+        "print function"
+        n = int(np.sqrt(len(self.atoms))) #define the ength of each side 
+        grid = self.atoms.reshape(n, n) #creates an NxN grid of atoms 
         output = ""
-        for row in grid:
+        for row in grid: 
             for point in row:
                 output += f"{point} "
             output += "\n"
-        
+
+        output += f"\nsimulated half life =  {round(self.time, 2)} minutes \npredicted half life = {round((np.log(2)/ (self.lamda/self.step)), 2)} minutes"        
         return output
 
     
@@ -23,23 +27,33 @@ class Atoms():
     def decay(self):
         """simulates decay of the atoms"""
         count = 0 
+        self.time = 0
 
 
         while count < len(self.atoms)//2: #loop until count is half of the total
+            self.time += self.step #add 1 to step 
             for i in range(len(self.atoms)): #loop for all atoms 
                 if self.atoms[i] != 0: #if atom hasnt already decayed
                     if random.random() < self.lamda: #decide if it should decay
                         self.atoms[i] = 0 # update to decayed state
                         count += 1 #update how many have decayed
+                       
                     else:
                        pass 
                 else:
                     pass
 
+        
+
        
 def main():
-    lamda = 0.2
-    array = Atoms(np.ones(25, dtype=int),  lamda)
+    lamda_per_min = 0.02775
+    step = 0.01
+    length = 50
+    lamdaSimulation = lamda_per_min * step 
+    n_of_atoms = length**2 #NxN length of array 
+
+    array = Atoms(np.ones(n_of_atoms, dtype=int),  lamdaSimulation, step) # initialises the atomsas an array of all ones 
     array.decay()
     print(array)
 
